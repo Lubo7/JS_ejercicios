45e5a719d5564263e841b92925f348137bb1fb6a
Kramoris <Lubomir_Kramoris@dell.com>
2021-10-18 18:48:30 +0200

    2_6 ejercicio anadido

--- 2_6/main.js
+++ 2_6/main.js
@@ -2,7 +2,22 @@
 // positivo (hay que controlarlo) pedido por prompt. Crea un método que realice 
 // esta acción, pasando el número por parámetro devolverá el número de cifras.
 
+let numero = prompt("Indica el numero que quieres calcular");
+let resultado = "";
 
+const cifras = (numero) => {
+	if(numero >=0 && !numero.includes("." || ",")) {
+		resultado = numero.toString().length;
+		return "El numero " + numero + " contiene " + resultado + " cifras.";
+	} else {
+		return "El numero no es entero positivo";
+	}
+}
+
+resultado = cifras(numero);
+alert(resultado);
+
+/*
 const calculaCifras = (numero) => {
 	const resultado = numero.toString().length;
 	return resultado;
@@ -12,4 +27,4 @@ const calculaCifras = (numero) => {
 let numero = parseInt(prompt('Introduzca un numero'));
 let resultado = calculaCifras(numero);
 
-alert('El resultado es ' + resultado);
+alert('El resultado es ' + resultado);*/
