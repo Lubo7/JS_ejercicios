34ff79f5a7c92df7efd9d0e3eca4ae3982bdfb29
Kramoris <Lubomir_Kramoris@dell.com>
2021-10-19 17:10:33 +0200

    2_10 ejercicio update

--- 2_10/main.js
+++ 2_10/main.js
@@ -3,4 +3,39 @@
 // es el mayor de todos. Haz un método para comprobar que el número aleatorio es
 // primo, puedes hacer todos los métodos que necesites.
 
+var tamaño = prompt("Introduce el tamaño del array");
+tamaño = parseInt(tamaño);
+var primo = true;
+var array = [];
+var num = 0;
 
+//Proceso
+
+const numRandom = () => {
+    num = Math.floor(Math.random() * (100 - 3) + 3);
+    return num;
+}
+
+const calculaPrimo = (valor) =>{
+    primo = true;
+    for (let a = 2; a < valor; a++) {
+        if (valor%a == 0) {
+            primo = false;
+        }
+    }
+}
+
+const añadirArray = () =>{
+    while (array.length < tamaño) {
+        calculaPrimo(numRandom());
+        if (primo) {
+            array.push(num);
+        } 
+    } 
+    console.log(array);
+    console.log(Math.max(...array));
+}
+
+//Output
+
+añadirArray();
