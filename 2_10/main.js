0070d02b2f2a050d102e5a8527aae62328e5523b
Kramoris <Lubomir_Kramoris@dell.com>
2021-10-19 17:08:13 +0200

    2_9 and 2_10 exercises updated

--- 2_10/main.js
+++ 2_10/main.js
@@ -2,3 +2,5 @@
 // números aleatorios primos entre los números deseados, por último, nos indicar cual
 // es el mayor de todos. Haz un método para comprobar que el número aleatorio es
 // primo, puedes hacer todos los métodos que necesites.
+
+
--- 2_9/main.js
+++ 2_9/main.js
@@ -3,3 +3,21 @@
 // el valor de cada posición y la suma de todos los valores. Tareas a realizar: Haz un
 // método para rellenar el array(que tenga como parámetros los números entre los que
 // tenga que generar) y otro para mostrar el contenido y la suma del array.
+
+let cant = prompt("Introduzca el cantidad de numeros en array");
+let arr = [];
+let sum = 0;
+const rand = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+const calc = () => {
+    for (let i = 0; i < cant; i++){
+        arr[i] = parseInt(rand(0,9));
+        sum += arr[i];
+        console.log("Numero de array en posicion " + i + " es: " + arr[i]);
+    }
+};
+
+console.log(arr);
+console.log("Suma del array : " + sum);
