4755b4824b7b6ad43c8a447d783b596279daeee5
Kramoris <Lubomir_Kramoris@dell.com>
2021-10-19 17:17:36 +0200

    ejercicios updated

--- 2_11/main.js
+++ 2_11/main.js
@@ -4,3 +4,51 @@
 // tenga como parámetros, los dos arrays y devuelva uno nuevo con la multiplicación
 // de la posición 0 del array1 con el del array2 y así sucesivamente. Por último, muestra
 // el contenido de cada array.
+
+
+//Input
+
+let num;
+
+let tamaño = prompt("Introduce el tamaño que quieres que tengan ambos arrays");
+
+let array1 = [];
+let array2 = [];
+let arrayCajon = [];
+
+//Proceso
+
+const numRandom = () => {
+    num = Math.floor(Math.random() * 100);
+    return num;
+};
+const rellenarArray = (array, tamaño) => {
+    while (array.length < tamaño) {
+        array.push(numRandom());
+    }
+};
+const guardarArray1EnArray2 = (array1, array2) =>{
+    for (let i = 0; i < array1.length; i++) {
+        array2.push(array1[i]);        
+    }
+}
+const sustituirValoresArray = (array) =>{
+    for (let i = 0; i < array.length; i++) {
+        array.shift();
+        array.push(numRandom());
+    }
+};
+const multiplicarValoresArray = (array1, array2) =>{
+    for (let i = 0; i < array1.length; i++) {
+        arrayCajon[i] = array1[i] * array2[i];        
+    }
+}
+
+rellenarArray(array1, tamaño);
+guardarArray1EnArray2(array1, array2);
+sustituirValoresArray(array2);
+multiplicarValoresArray(array1, array2);
+
+//Output
+
+console.log("Array 1: " + array1 + "\nArray 2: " + array2 + "\nArray multiplicación: " + arrayCajon);
--- 2_12/main.js
+++ 2_12/main.js
@@ -4,3 +4,56 @@
 // número correcto), estos deben guardarse en un nuevo array. Por ejemplo, en un
 // array de 10 posiciones le indicamos mostrar los números acabados en 5, podría salir
 // 155, 25, etc.
+
+
+//Input
+
+var num;
+
+var tamaño = prompt("Introduce el tamaño del array");
+tamaño = parseInt(tamaño);
+
+var array = [];
+var arrayConUltimoDigito = [];
+
+var ultimoDigito = prompt("Introduce un número del 0 al 9. \nEste número será el que usaré para seleccionar los números del array creado.\nTodos los números que acaben en ese número serán los seleccionados.\nSi intentas introducir un número de una cifra pero con decimales, se redondeará");
+ultimoDigito = parseInt(ultimoDigito);
+
+while (isNaN(ultimoDigito) || ultimoDigito > 9) {
+    var ultimoDigito = prompt("Has introducido mal el número. Prueba de nuevo");
+    ultimoDigito = parseInt(ultimoDigito);
+}
+
+//Proceso
+
+const numRandom = () => {
+    num = Math.floor(Math.random() * (301 - 1) + 1);
+    return num;
+};
+
+const rellenarArray = (array) =>{
+    for (let i = 0; i < tamaño; i++) {
+        array.push(numRandom());        
+    }
+}
+
+const saberUltimoDigito = (numero) =>{
+    let digito = numero % 10;
+    return digito;
+}
+
+const crearArrayUltimoDigito = () =>{
+    array.map(function(numero) {
+        if (saberUltimoDigito(numero) == ultimoDigito) {
+            arrayConUltimoDigito.push(numero);
+        }
+    })
+}
+
+//Output
+
+rellenarArray(array);
+console.log("Este es el primer array: " + array);
+crearArrayUltimoDigito();
+console.log("Este es el número que elegiste para filtrar los números del nuevo array: " + ultimoDigito);
+console.log("Este es el nuevo array: " + arrayConUltimoDigito);
